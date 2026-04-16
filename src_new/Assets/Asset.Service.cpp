a2930c23fbee4de86e97bfe5a67537c5c27c7c99
intrinsicD <intrinsicd@posteo.de>
2026-04-16 23:26:29 +0200

    update. forgot to commit Asset.Service.cpp

--- src_new/Assets/Asset.Service.cpp
+++ src_new/Assets/Asset.Service.cpp
@@ -1,18 +1,94 @@
 module;
 
+#include <mutex>
+
 module Extrinsic.Asset.Service;
 
 namespace Extrinsic::Assets
 {
+    AssetService::AssetService()
+    {
+        m_LoadPipeline.BindRegistry(&m_Registry);
+        m_LoadPipeline.BindEventBus(&m_EventBus);
+    }
+
     Core::Result AssetService::Reload(AssetId id)
     {
+        if (!m_Registry.IsAlive(id))
+        {
+            return Core::Err(Core::ErrorCode::ResourceNotFound);
+        }
+
+        auto toUnloaded = m_Registry.SetState(id, AssetState::Ready, AssetState::Unloaded);
+        if (!toUnloaded.has_value())
+        {
+            return toUnloaded;
+        }
+
+        std::string path;
+        {
+            std::scoped_lock lock(m_PathMutex);
+            const auto pathIt = m_PathById.find(id);
+            if (pathIt == m_PathById.end())
+            {
+                return Core::Err(Core::ErrorCode::ResourceNotFound);
+            }
+            path = pathIt->second;
+        }
+
+        const auto meta = m_Registry.GetMeta(id);
+        if (!meta.has_value())
+        {
+            return std::unexpected(meta.error());
+        }
+
+        LoadRequest req{.id = id, .typeId = meta->typeId, .path = path.c_str(), .needsGpuUpload = false};
+        auto result = m_LoadPipeline.EnqueueIO(req);
+        if (!result.has_value())
+        {
+            return result;
+        }
+
+        m_EventBus.Publish(id, AssetEvent::Reloaded);
+        return Core::Ok();
     }
 
     Core::Result AssetService::Destroy(AssetId id)
     {
+        if (!m_Registry.IsAlive(id))
+        {
+            return Core::Err(Core::ErrorCode::ResourceNotFound);
+        }
+
+        std::string path;
+        {
+            std::scoped_lock lock(m_PathMutex);
+            const auto pathIt = m_PathById.find(id);
+            if (pathIt != m_PathById.end())
+            {
+                path = pathIt->second;
+                m_PathById.erase(pathIt);
+            }
+        }
+
+        if (!path.empty())
+        {
+            (void)m_PathIndex.Erase(path, id);
+        }
+
+        (void)m_PayloadStore.Retire(id);
+
+        if (auto destroyed = m_Registry.Destroy(id); !destroyed.has_value())
+        {
+            return destroyed;
+        }
+
+        m_EventBus.Publish(id, AssetEvent::Destroyed);
+        return Core::Ok();
     }
 
     void AssetService::Tick()
     {
+        m_EventBus.Flush();
     }
 }
