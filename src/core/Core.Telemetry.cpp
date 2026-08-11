20e58c6a00c1c85c0d1f8b05a02e026c0da3a078
dieckman <dieckman@cs.uni-bonn.de>
2026-08-11 15:56:54 +0200

    fixed formatting of Telemetry.cpp file

--- src/core/Core.Telemetry.cpp
+++ src/core/Core.Telemetry.cpp
@@ -26,8 +26,8 @@ namespace Extrinsic::Core::Telemetry
     {
         void RecordAlloc(std::size_t bytes) noexcept
         {
-            g_AllocBytes.fetch_add(bytes,        std::memory_order_relaxed);
-            g_AllocCount.fetch_add(1,            std::memory_order_relaxed);
+            g_AllocBytes.fetch_add(bytes, std::memory_order_relaxed);
+            g_AllocCount.fetch_add(1, std::memory_order_relaxed);
         }
 
         uint64_t SnapshotBytes() noexcept { return g_AllocBytes.load(std::memory_order_relaxed); }
@@ -55,7 +55,7 @@ namespace Extrinsic::Core::Telemetry
     {
         if (CallCount == 0) return 0.0;
         return static_cast<double>(TotalTimeNs) /
-               static_cast<double>(CallCount) / 1'000'000.0;
+            static_cast<double>(CallCount) / 1'000'000.0;
     }
 
     double TimingCategory::TotalMs() const noexcept
@@ -66,9 +66,9 @@ namespace Extrinsic::Core::Telemetry
     void TimingCategory::Reset() noexcept
     {
         TotalTimeNs = 0;
-        CallCount   = 0;
-        MinTimeNs   = UINT64_MAX;
-        MaxTimeNs   = 0;
+        CallCount = 0;
+        MinTimeNs = UINT64_MAX;
+        MaxTimeNs = 0;
     }
 
     // -----------------------------------------------------------------------
@@ -83,15 +83,15 @@ namespace Extrinsic::Core::Telemetry
     void TelemetrySystem::BeginFrame()
     {
         m_FrameStart = std::chrono::high_resolution_clock::now();
-        m_SampleCount.store(0,  std::memory_order_relaxed);
-        m_DrawCalls.store(0,    std::memory_order_relaxed);
-        m_Triangles.store(0,    std::memory_order_relaxed);
-        m_FenceWaitNs.store(0,  std::memory_order_relaxed);
-        m_AcquireNs.store(0,    std::memory_order_relaxed);
-        m_PresentNs.store(0,    std::memory_order_relaxed);
+        m_SampleCount.store(0, std::memory_order_relaxed);
+        m_DrawCalls.store(0, std::memory_order_relaxed);
+        m_Triangles.store(0, std::memory_order_relaxed);
+        m_FenceWaitNs.store(0, std::memory_order_relaxed);
+        m_AcquireNs.store(0, std::memory_order_relaxed);
+        m_PresentNs.store(0, std::memory_order_relaxed);
         m_SimTickCount.store(0, std::memory_order_relaxed);
-        m_SimClampCount.store(0,std::memory_order_relaxed);
-        m_SimCpuNs.store(0,     std::memory_order_relaxed);
+        m_SimClampCount.store(0, std::memory_order_relaxed);
+        m_SimCpuNs.store(0, std::memory_order_relaxed);
 
         const std::size_t count = m_CategoryCount.load(std::memory_order_relaxed);
         for (std::size_t i = 0; i < count; ++i)
@@ -100,44 +100,44 @@ namespace Extrinsic::Core::Telemetry
 
     void TelemetrySystem::EndFrame()
     {
-        const auto end      = std::chrono::high_resolution_clock::now();
-        const uint64_t ns   = static_cast<uint64_t>(
+        const auto end = std::chrono::high_resolution_clock::now();
+        const uint64_t ns = static_cast<uint64_t>(
             std::chrono::duration_cast<std::chrono::nanoseconds>(end - m_FrameStart).count());
 
         const std::size_t idx = m_CurrentFrame % kMaxFrameHistory;
         auto& f = m_FrameHistory[idx];
-        f.FrameNumber         = m_CurrentFrame;
-        f.FrameTimeNs         = ns;
-        f.CpuTimeNs           = ns;
-        f.FenceWaitTimeNs     = m_FenceWaitNs.load(std::memory_order_relaxed);
-        f.AcquireTimeNs       = m_AcquireNs.load(std::memory_order_relaxed);
-        f.PresentTimeNs       = m_PresentNs.load(std::memory_order_relaxed);
+        f.FrameNumber = m_CurrentFrame;
+        f.FrameTimeNs = ns;
+        f.CpuTimeNs = ns;
+        f.FenceWaitTimeNs = m_FenceWaitNs.load(std::memory_order_relaxed);
+        f.AcquireTimeNs = m_AcquireNs.load(std::memory_order_relaxed);
+        f.PresentTimeNs = m_PresentNs.load(std::memory_order_relaxed);
         f.FramesInFlightCount = m_FramesInFlight.load(std::memory_order_relaxed);
-        f.DrawCalls           = m_DrawCalls.load(std::memory_order_relaxed);
-        f.TriangleCount       = m_Triangles.load(std::memory_order_relaxed);
-        f.SampleCount         = m_SampleCount.load(std::memory_order_relaxed);
+        f.DrawCalls = m_DrawCalls.load(std::memory_order_relaxed);
+        f.TriangleCount = m_Triangles.load(std::memory_order_relaxed);
+        f.SampleCount = m_SampleCount.load(std::memory_order_relaxed);
         f.SimulationTickCount = m_SimTickCount.load(std::memory_order_relaxed);
         f.SimulationClampHitCount = m_SimClampCount.load(std::memory_order_relaxed);
         f.SimulationCpuTimeNs = m_SimCpuNs.load(std::memory_order_relaxed);
-        f.AllocBytes          = Alloc::SnapshotBytes();
-        f.AllocCount          = Alloc::SnapshotCount();
-        f.FrameGraphCompileTimeNs     = m_FGCompileNs.load(std::memory_order_relaxed);
-        f.FrameGraphExecuteTimeNs     = m_FGExecuteNs.load(std::memory_order_relaxed);
+        f.AllocBytes = Alloc::SnapshotBytes();
+        f.AllocCount = Alloc::SnapshotCount();
+        f.FrameGraphCompileTimeNs = m_FGCompileNs.load(std::memory_order_relaxed);
+        f.FrameGraphExecuteTimeNs = m_FGExecuteNs.load(std::memory_order_relaxed);
         f.FrameGraphCriticalPathTimeNs = m_FGCriticalPathNs.load(std::memory_order_relaxed);
         // Task stats
-        f.Tasks.ParkCount          = m_TaskParkCount.load(std::memory_order_relaxed);
-        f.Tasks.UnparkCount        = m_TaskUnparkCount.load(std::memory_order_relaxed);
-        f.Tasks.ParkLatencyP50Ns   = m_TaskParkP50Ns.load(std::memory_order_relaxed);
-        f.Tasks.ParkLatencyP95Ns   = m_TaskParkP95Ns.load(std::memory_order_relaxed);
-        f.Tasks.ParkLatencyP99Ns   = m_TaskParkP99Ns.load(std::memory_order_relaxed);
+        f.Tasks.ParkCount = m_TaskParkCount.load(std::memory_order_relaxed);
+        f.Tasks.UnparkCount = m_TaskUnparkCount.load(std::memory_order_relaxed);
+        f.Tasks.ParkLatencyP50Ns = m_TaskParkP50Ns.load(std::memory_order_relaxed);
+        f.Tasks.ParkLatencyP95Ns = m_TaskParkP95Ns.load(std::memory_order_relaxed);
+        f.Tasks.ParkLatencyP99Ns = m_TaskParkP99Ns.load(std::memory_order_relaxed);
         f.Tasks.UnparkLatencyP50Ns = m_TaskUnparkP50Ns.load(std::memory_order_relaxed);
         f.Tasks.UnparkLatencyP95Ns = m_TaskUnparkP95Ns.load(std::memory_order_relaxed);
         f.Tasks.UnparkLatencyP99Ns = m_TaskUnparkP99Ns.load(std::memory_order_relaxed);
         f.Tasks.UnparkLatencyTailSpreadNs = m_TaskUnparkTailNs.load(std::memory_order_relaxed);
-        f.Tasks.IdleWaitCount      = m_TaskIdleWaitCount.load(std::memory_order_relaxed);
-        f.Tasks.IdleWaitTotalNs    = m_TaskIdleWaitNs.load(std::memory_order_relaxed);
+        f.Tasks.IdleWaitCount = m_TaskIdleWaitCount.load(std::memory_order_relaxed);
+        f.Tasks.IdleWaitTotalNs = m_TaskIdleWaitNs.load(std::memory_order_relaxed);
         f.Tasks.QueueContentionCount = m_TaskContentionCount.load(std::memory_order_relaxed);
-        f.Tasks.StealSuccessRatio  = m_TaskStealRatio.load(std::memory_order_relaxed);
+        f.Tasks.StealSuccessRatio = m_TaskStealRatio.load(std::memory_order_relaxed);
 
         Alloc::Reset();
         ++m_CurrentFrame;
@@ -155,15 +155,15 @@ namespace Extrinsic::Core::Telemetry
         {
             const std::size_t fi = m_CurrentFrame % kMaxFrameHistory;
             auto& s = m_SampleBuffer[fi * kMaxSamplesPerFrame + si];
-            s.NameHash   = nameHash;
+            s.NameHash = nameHash;
             s.DurationNs = durationNs;
-            s.Depth      = depth;
+            s.Depth = depth;
         }
     }
 
     void TelemetrySystem::RecordDrawCall(uint32_t triangles) noexcept
     {
-        m_DrawCalls.fetch_add(1,         std::memory_order_relaxed);
+        m_DrawCalls.fetch_add(1, std::memory_order_relaxed);
         m_Triangles.fetch_add(triangles, std::memory_order_relaxed);
     }
 
@@ -177,9 +177,9 @@ namespace Extrinsic::Core::Telemetry
                                             uint64_t acquireNs,
                                             uint64_t presentNs) noexcept
     {
-        m_FenceWaitNs.store(fenceNs,   std::memory_order_relaxed);
-        m_AcquireNs.store(acquireNs,   std::memory_order_relaxed);
-        m_PresentNs.store(presentNs,   std::memory_order_relaxed);
+        m_FenceWaitNs.store(fenceNs, std::memory_order_relaxed);
+        m_AcquireNs.store(acquireNs, std::memory_order_relaxed);
+        m_PresentNs.store(presentNs, std::memory_order_relaxed);
     }
 
     void TelemetrySystem::SetFramesInFlightCount(uint32_t count) noexcept
@@ -193,32 +193,32 @@ namespace Extrinsic::Core::Telemetry
     {
         m_SimTickCount.store(tickCount, std::memory_order_relaxed);
         m_SimClampCount.store(clampCount, std::memory_order_relaxed);
-        m_SimCpuNs.store(cpuNs,          std::memory_order_relaxed);
+        m_SimCpuNs.store(cpuNs, std::memory_order_relaxed);
     }
 
     void TelemetrySystem::SetTaskStats(const TaskSchedulerStats& s) noexcept
     {
-        m_TaskParkCount.store(s.ParkCount,               std::memory_order_relaxed);
-        m_TaskUnparkCount.store(s.UnparkCount,           std::memory_order_relaxed);
-        m_TaskParkP50Ns.store(s.ParkLatencyP50Ns,        std::memory_order_relaxed);
-        m_TaskParkP95Ns.store(s.ParkLatencyP95Ns,        std::memory_order_relaxed);
-        m_TaskParkP99Ns.store(s.ParkLatencyP99Ns,        std::memory_order_relaxed);
-        m_TaskUnparkP50Ns.store(s.UnparkLatencyP50Ns,    std::memory_order_relaxed);
-        m_TaskUnparkP95Ns.store(s.UnparkLatencyP95Ns,    std::memory_order_relaxed);
-        m_TaskUnparkP99Ns.store(s.UnparkLatencyP99Ns,    std::memory_order_relaxed);
+        m_TaskParkCount.store(s.ParkCount, std::memory_order_relaxed);
+        m_TaskUnparkCount.store(s.UnparkCount, std::memory_order_relaxed);
+        m_TaskParkP50Ns.store(s.ParkLatencyP50Ns, std::memory_order_relaxed);
+        m_TaskParkP95Ns.store(s.ParkLatencyP95Ns, std::memory_order_relaxed);
+        m_TaskParkP99Ns.store(s.ParkLatencyP99Ns, std::memory_order_relaxed);
+        m_TaskUnparkP50Ns.store(s.UnparkLatencyP50Ns, std::memory_order_relaxed);
+        m_TaskUnparkP95Ns.store(s.UnparkLatencyP95Ns, std::memory_order_relaxed);
+        m_TaskUnparkP99Ns.store(s.UnparkLatencyP99Ns, std::memory_order_relaxed);
         m_TaskUnparkTailNs.store(s.UnparkLatencyTailSpreadNs, std::memory_order_relaxed);
-        m_TaskIdleWaitCount.store(s.IdleWaitCount,       std::memory_order_relaxed);
-        m_TaskIdleWaitNs.store(s.IdleWaitTotalNs,        std::memory_order_relaxed);
+        m_TaskIdleWaitCount.store(s.IdleWaitCount, std::memory_order_relaxed);
+        m_TaskIdleWaitNs.store(s.IdleWaitTotalNs, std::memory_order_relaxed);
         m_TaskContentionCount.store(s.QueueContentionCount, std::memory_order_relaxed);
-        m_TaskStealRatio.store(s.StealSuccessRatio,      std::memory_order_relaxed);
+        m_TaskStealRatio.store(s.StealSuccessRatio, std::memory_order_relaxed);
     }
 
     void TelemetrySystem::SetFrameGraphTimings(uint64_t compileNs,
                                                uint64_t executeNs,
                                                uint64_t criticalPathNs) noexcept
     {
-        m_FGCompileNs.store(compileNs,       std::memory_order_relaxed);
-        m_FGExecuteNs.store(executeNs,       std::memory_order_relaxed);
+        m_FGCompileNs.store(compileNs, std::memory_order_relaxed);
+        m_FGExecuteNs.store(executeNs, std::memory_order_relaxed);
         m_FGCriticalPathNs.store(criticalPathNs, std::memory_order_relaxed);
     }
 
@@ -240,7 +240,12 @@ namespace Extrinsic::Core::Telemetry
             bool found = false;
             for (auto& e : m_PassTimings)
             {
-                if (e.Name == name) { e.CpuTimeNs = ns; found = true; break; }
+                if (e.Name == name)
+                {
+                    e.CpuTimeNs = ns;
+                    found = true;
+                    break;
+                }
             }
             if (!found)
                 m_PassTimings.push_back(PassTimingEntry{name, 0, ns});
@@ -250,8 +255,8 @@ namespace Extrinsic::Core::Telemetry
     const FrameStats& TelemetrySystem::GetFrameStats(std::size_t framesAgo) const noexcept
     {
         const std::size_t idx = (m_CurrentFrame > 0
-            ? (m_CurrentFrame - 1 - framesAgo) % kMaxFrameHistory
-            : 0);
+                                     ? (m_CurrentFrame - 1 - framesAgo) % kMaxFrameHistory
+                                     : 0);
         return m_FrameHistory[idx];
     }
 
@@ -280,8 +285,10 @@ namespace Extrinsic::Core::Telemetry
             if (m_Categories[i].CallCount > 0)
                 result.push_back(&m_Categories[i]);
         std::sort(result.begin(), result.end(),
-            [](const TimingCategory* a, const TimingCategory* b)
-            { return a->TotalTimeNs > b->TotalTimeNs; });
+                  [](const TimingCategory* a, const TimingCategory* b)
+                  {
+                      return a->TotalTimeNs > b->TotalTimeNs;
+                  });
         return result;
     }
 
@@ -298,7 +305,7 @@ namespace Extrinsic::Core::Telemetry
             if (newIdx < kMaxCategories)
             {
                 m_Categories[newIdx].NameHash = nameHash;
-                m_Categories[newIdx].Name     = name;
+                m_Categories[newIdx].Name = name;
                 return newIdx;
             }
         }
@@ -307,9 +314,10 @@ namespace Extrinsic::Core::Telemetry
 
     ScopedTimer::ScopedTimer(const char* name, uint32_t nameHash) noexcept
         : m_Name(name)
-        , m_NameHash(nameHash)
-        , m_Start(std::chrono::high_resolution_clock::now())
-    {}
+          , m_NameHash(nameHash)
+          , m_Start(std::chrono::high_resolution_clock::now())
+    {
+    }
 
     ScopedTimer::~ScopedTimer()
     {
